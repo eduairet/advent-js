9cd6268159878aa722f33eec4f27077f25fb4537
eduairet <hola@eduairet.com>
2023-12-05 22:19:12 -0600

    Small fixes

--- 2023/day05.ts
+++ 2023/day05.ts
@@ -1,8 +1,7 @@
-export function cyberReindeer(road: string, time: number): Array<string> {
+export function cyberReindeer(road: string, time: number): string[] {
     const result = [road];
-    let emptyRoad = road.replace('S', '.');
-    let [i, position] = [0, 0];
-    while (time > 1) {
+    let [emptyRoad, i, position] = [`.${road.slice(1)}`, 0, 0];
+    while (time-- > 1) {
         i++;
         if (i === 5) emptyRoad = emptyRoad.replace(/\|/g, '*');
         if (emptyRoad[position + 1] !== '|') position++;
@@ -11,7 +10,6 @@ export function cyberReindeer(road: string, time: number): Array<string> {
             emptyRoad.substring(position + 1),
         ];
         result.push(`${left}S${right}`);
-        time--;
     }
     return result;
 }
