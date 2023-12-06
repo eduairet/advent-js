a0c1c24d9fa46b80f4b91555543c4e9013ba44a4
eduairet <hola@eduairet.com>
2023-12-05 22:07:34 -0600

    Small fixes

--- 2023/day05.ts
+++ 2023/day05.ts
@@ -1,12 +1,16 @@
 export function cyberReindeer(road: string, time: number): Array<string> {
     const result = [road];
-    const emptyRoad = Array.from(road).map((char) => (char === 'S' ? '.' : char));
-    let position = 0;
+    let emptyRoad = road.replace('S', '.');
+    let [i, position] = [0, 0];
     while (time > 1) {
+        i++;
+        if (i === 5) emptyRoad = emptyRoad.replace(/\|/g, '*');
         if (emptyRoad[position + 1] !== '|') position++;
-        const left = emptyRoad.slice(0, position);
-        const right = emptyRoad.slice(position + 1);
-        result.push(`${left.join('')}S${right.join('')}`);
+        const [left, right] = [
+            emptyRoad.substring(0, position),
+            emptyRoad.substring(position + 1),
+        ];
+        result.push(`${left}S${right}`);
         time--;
     }
     return result;
