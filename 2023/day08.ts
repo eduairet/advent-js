9819f21e71e732a6880240d4f7120f25d6634c44
eduairet <hola@eduairet.com>
2023-12-08 20:16:01 -0600

    08

--- 2023/day08.ts
+++ 2023/day08.ts
@@ -1,24 +1,18 @@
-export function organizeGifts(gifts: string) {
-    const groups = gifts.split(/(\d+)([a-z])/).filter(Boolean);
-    const formedGroups = groups.reduce<[number, string][]>((a, _, i, arr) => {
-        if (i % 2 === 0) {
-            const group = arr.slice(i, i + 2);
-            a.push([+group[0], group[1]]);
-        }
-        return a;
-    }, []);
-    let result: string[] = [];
-    for (const group of formedGroups) {
-        const [count, symbol] = group;
-        const totalBoxes = Math.floor(count / 10);
-        const pallets = Math.floor(totalBoxes / 5);
-        const boxes = totalBoxes % 5;
+export function organizeGifts(gifts: string): string {
+    const groups = gifts.match(/\d+[a-z]/g) || [];
+    const result = [];
+    for (const group of groups) {
+        const [count, symbol] = [
+            +group.match(/\d+/)[0],
+            group.match(/[a-z]/)[0],
+        ];
+
+        const pallets = Math.floor(count / 50);
+        const boxes = Math.floor((count % 50) / 10);
         const additional = count % 10;
-        let pile = '';
-        if (pallets > 0) pile += `[${symbol}]`.repeat(pallets);
-        if (boxes > 0) pile += `{${symbol}}`.repeat(boxes);
-        if (additional > 0) pile += `(${symbol.repeat(additional)})`;
-        result.push(pile);
+
+        result.push(`[${symbol}]`.repeat(pallets));
+        result.push(`{${symbol}}`.repeat(boxes));
+        if (additional > 0) result.push(`(${symbol.repeat(additional)})`);
     }
     return result.join('');
-}
