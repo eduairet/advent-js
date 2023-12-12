f5f2960e294309ddfe210f557a15711dc9fdbb39
eduairet <hola@eduairet.com>
2023-12-11 23:43:27 -0600

    11 improved

--- 2023/day11.ts
+++ 2023/day11.ts
@@ -1,11 +1,12 @@
 export function getIndexsForPalindrome(word: string) {
     if (word === [...word].reverse().join('')) return [];
     let wordArr = [...word];
+    const temp = [...wordArr];
     for (const [i, l1] of wordArr.entries()) {
         for (const [j, l2] of wordArr.entries()) {
-            const temp = [...wordArr];
             [temp[j], temp[i]] = [l1, l2];
-            if (temp.join('') === temp.reverse().join('')) return [i, j];
+            if (temp.join('') === [...temp].reverse().join('')) return [i, j];
+            [temp[j], temp[i]] = [l2, l1];
         }
     }
     return null;
