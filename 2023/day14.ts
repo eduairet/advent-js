7897d14d49d0e61681be3077fc45e6c38d9e2bc5
eduairet <hola@eduairet.com>
2023-12-14 22:27:39 -0600

    14

--- 2023/day14.ts
+++ 2023/day14.ts
@@ -1,10 +1,11 @@
 export function maxGifts(houses: number[]) {
     const { length: n } = houses;
-    let res: number[] = Array(n).fill(0);
-    [res[0], res[1]] = [houses[0], Math.max(...houses.slice(0, 2))];
+    let prevMax = houses[0];
+    let currMax = Math.max(...houses.slice(0, 2));
     for (let i = 2; i < n; i++) {
-        const [o1, o2] = [res[i - 1], res[i - 2] + houses[i]];
-        res[i] = Math.max(o1, o2);
+        let newMax = Math.max(currMax, prevMax + houses[i]);
+        prevMax = currMax;
+        currMax = newMax;
     }
-    return res.pop();
+    return currMax;
 }
