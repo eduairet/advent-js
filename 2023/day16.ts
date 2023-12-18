740a208537308cd4538981c2e90cb242dc96ad96
eduairet <hola@eduairet.com>
2023-12-18 14:35:57 -0600

    16 improved

--- 2023/day16.ts
+++ 2023/day16.ts
@@ -1,24 +1,18 @@
 export function transformTree(tree: (number | null)[]) {
-    if (tree.length === 0 || tree[0] === null) return null;
     interface Node {
         value: number | null;
         left: Node | null;
         right: Node | null;
     }
-    const newNode = (value: number | null): Node => {
-        return {
-            value,
-            left: null,
-            right: null,
-        };
-    };
     let root: Node | null = null;
     const addNode = (tree: (number | null)[], i: number): Node | null => {
         let root = null;
         if (i < tree.length) {
-            root = newNode(tree[i]);
+            root = { value: tree[i], left: null, right: null };
             if (root.value === null) return null;
+            // @ts-ignore
             root.left = addNode(tree, 2 * i + 1);
+            // @ts-ignore
             root.right = addNode(tree, 2 * i + 2);
         }
         return root;
