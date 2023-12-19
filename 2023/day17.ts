fa0c0cf8495e0335680cbaa10e0c311de82bc3bc
eduairet <hola@eduairet.com>
2023-12-19 00:01:02 -0600

    17

--- 2023/day17.ts
+++ 2023/day17.ts
@@ -1,3 +1,15 @@
 export function optimizeIntervals(intervals: number[][]) {
-    return [];
+    intervals.sort((a, b) => a[0] - b[0]);
+    let i = 1;
+    while (i < intervals.length) {
+        const [currentStart, currentEnd] = intervals[i];
+        const [_, lastEnd] = intervals[i - 1];
+
+        if (currentStart > lastEnd) i++;
+        else {
+            intervals[i - 1][1] = Math.max(currentEnd, lastEnd);
+            intervals.splice(i, 1);
+        }
+    }
+    return intervals;
 }
