8f786ebc79ddb2be8d8eeed1c5af049f2ceb156a
eduairet <hola@eduairet.com>
2023-12-20 13:55:33 -0600

    20 improved

--- 2023/day20.ts
+++ 2023/day20.ts
@@ -1,28 +1,28 @@
 export function distributeGifts(weights: (number | null)[][]) {
-    const isValidNeighbor = ([x, y]: number[]) => {
-        const xSafe = x >= 0 && x < weights[0].length;
-        const ySafe = y >= 0 && y < weights.length;
-        return xSafe && ySafe;
-    };
-    const distributed: number[][] = weights.map(row => row.map(v => v ?? 0));
-    for (const [y, row] of weights.entries()) {
-        for (let x = 0; x < row.length; x++) {
+    const n = weights.length;
+    const m = weights[0].length;
+    const distributed: number[][] = new Array(n);
+    for (let y = 0; y < n; y++) {
+        distributed[y] = new Array(m);
+        for (let x = 0; x < m; x++) {
+            let sum = weights[y][x] ?? 0;
+            let count = Number(!!weights[y][x]);
             const neighbors = [
                 [x - 1, y],
                 [x + 1, y],
                 [x, y - 1],
                 [x, y + 1],
-            ].filter(isValidNeighbor);
-            let count = neighbors.length + (weights[y][x] !== null ? 1 : 0);
+            ];
             for (const [nx, ny] of neighbors) {
-                let neighbor = weights[ny][nx];
-                if (neighbor === null) {
-                    count--;
-                    continue;
+                const xSafe = nx >= 0 && nx < m;
+                const ySafe = ny >= 0 && ny < n;
+                const validCoord = xSafe && ySafe;
+                if (validCoord && weights[ny][nx] !== null) {
+                    sum += weights[ny][nx]!;
+                    count++;
                 }
-                distributed[y][x] += neighbor;
             }
-            distributed[y][x] = Math.round(distributed[y][x] / count);
+            distributed[y][x] = Math.round(sum / count);
         }
     }
     return distributed;
