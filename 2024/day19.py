d1392c33d40c65f08e497b6abbaf2a027705c12f
eduairet <hola@eduairet.com>
2025-01-02 22:22:56 -0600

    Day 19

--- 2024/day19.py
+++ 2024/day19.py
@@ -6,31 +6,14 @@ def distribute_weight(weight):
         10: [" _________ ", "|         |", "|_________|"],
     }
 
-    if weight in box_sizes:
-        return "\n".join([line.rstrip() for line in box_sizes[weight]])
-
     stack_sizes = {
-        3: [box_sizes[1][0], f"{box_sizes[1][1]}_", box_sizes[2][1]],
+        3: box_sizes[1][:-1] + ["|_|_"] + [box_sizes[2][1]],
         4: [box_sizes[2][0], box_sizes[2][1], box_sizes[2][1]],
-        6: [box_sizes[1][0], f"{box_sizes[1][1]}___", box_sizes[5][1], box_sizes[5][2]],
-        7: [box_sizes[2][0], f"{box_sizes[2][1]}_", box_sizes[5][1], box_sizes[5][2]],
-        8: [
-            box_sizes[1][0],
-            f"{box_sizes[1][1]}_",
-            f"{box_sizes[2][1]}_",
-            box_sizes[5][1],
-            box_sizes[5][2],
-        ],
-        9: [
-            box_sizes[2][0],
-            box_sizes[2][1],
-            f"{box_sizes[2][1]}_",
-            box_sizes[5][1],
-            box_sizes[5][2],
-        ],
+        6: box_sizes[1][:-1] + ["|_|___"] + box_sizes[5][1:],
+        7: box_sizes[2][:-1] + ["|___|_"] + box_sizes[5][1:],
+        8: box_sizes[1][:-1] + ["|_|_", "|___|_"] + box_sizes[5][1:],
+        9: box_sizes[2] + [box_sizes[2][1] + "_"] + box_sizes[5][1:],
     }
-
-    result = []
 
     def build_stack(w):
         if w in stack_sizes:
@@ -38,17 +21,15 @@ def distribute_weight(weight):
         elif w in box_sizes:
             return box_sizes[w]
         elif w > 10:
-            base_boxes = w // 10
-            top = build_stack(w - base_boxes * 10)
-            bottom = []
-            for _ in range(base_boxes):
-                bottom.extend(box_sizes[10][1:])
-            return (
-                [*top[:-1], top[-1].ljust(10, "_"), *bottom]
-                if top
-                else [box_sizes[10][0], *bottom]
-            )
+            base, rem = divmod(w, 10)
+            top = build_stack(rem)
+            bottom = box_sizes[10][1:] * base
+            if top:
+                top[-1] = top[-1].ljust(10, "_")
+            else:
+                top = [box_sizes[10][0]]
+            return top + bottom
+        return []
 
-    result = build_stack(weight)
-
-    return "\n".join([line.rstrip() for line in result])
+    result = "\n".join(line for line in build_stack(weight))
+    return result
--- 2024/test_day19.py
+++ 2024/test_day19.py
@@ -4,18 +4,18 @@ from day19 import distribute_weight
 
 class TestDistributeWeight(unittest.TestCase):
     def test_distribute_weight(self):
-        self.assertEqual(distribute_weight(1), " _\n|_|")
-        self.assertEqual(distribute_weight(2), " ___\n|___|")
-        self.assertEqual(distribute_weight(3), " _\n|_|_\n|___|")
-        self.assertEqual(distribute_weight(4), " ___\n|___|\n|___|")
-        self.assertEqual(distribute_weight(5), " _____\n|     |\n|_____|")
-        self.assertEqual(distribute_weight(6), " _\n|_|___\n|     |\n|_____|")
-        self.assertEqual(distribute_weight(7), " ___\n|___|_\n|     |\n|_____|")
-        self.assertEqual(distribute_weight(8), " _\n|_|_\n|___|_\n|     |\n|_____|")
-        self.assertEqual(distribute_weight(9), " ___\n|___|\n|___|_\n|     |\n|_____|")
-        self.assertEqual(distribute_weight(10), " _________\n|         |\n|_________|")
+        self.assertEqual(distribute_weight(1), " _ \n|_|")
+        self.assertEqual(distribute_weight(2), " ___ \n|___|")
+        self.assertEqual(distribute_weight(3), " _ \n|_|_\n|___|")
+        self.assertEqual(distribute_weight(4), " ___ \n|___|\n|___|")
+        self.assertEqual(distribute_weight(5), " _____ \n|     |\n|_____|")
+        self.assertEqual(distribute_weight(6), " _ \n|_|___\n|     |\n|_____|")
+        self.assertEqual(distribute_weight(7), " ___ \n|___|_\n|     |\n|_____|")
+        self.assertEqual(distribute_weight(8), " _ \n|_|_\n|___|_\n|     |\n|_____|")
+        self.assertEqual(distribute_weight(9), " ___ \n|___|\n|___|_\n|     |\n|_____|")
+        self.assertEqual(distribute_weight(10), " _________ \n|         |\n|_________|")
         self.assertEqual(
-            distribute_weight(11), " _\n|_|_______\n|         |\n|_________|"
+            distribute_weight(11), " _ \n|_|_______\n|         |\n|_________|"
         )
 
 
