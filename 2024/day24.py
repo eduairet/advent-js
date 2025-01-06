a5f8926232775c1358cc108a8376f529c40df69a
eduairet <hola@eduairet.com>
2025-01-05 23:53:22 -0600

    Day 24

--- 2024/day24.py
+++ 2024/day24.py
@@ -1,2 +1,8 @@
 def is_trees_synchronized(tree1, tree2):
-    return [False, ""]
+    if tree1["value"] != tree2["value"]:
+        return [False, tree1["value"]]
+    if "left" in tree1:
+        left_sync, _ = is_trees_synchronized(tree1["left"], tree2["right"])
+        right_sync, _ = is_trees_synchronized(tree1["right"], tree2["left"])
+        return [left_sync and right_sync, tree1["value"]]
+    return [True, tree1["value"]]
